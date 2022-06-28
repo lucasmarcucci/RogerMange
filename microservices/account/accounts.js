daf00e9a883dd17e9c7057ce39813695b1f026e5
thomas_montet <thomas.montet@viacesi.fr>
2022-06-28 10:53:10 +0200

    sequilize post

--- microservices/account/accounts.js
+++ microservices/account/accounts.js
@@ -1,12 +1,7 @@
 const express = require("express");
-const mysql = require("mysql");
-const app = express();
-app.use(express.json());
-require("dotenv").config();
 
 //database mysql
 const db = require("./config/database");
-const Account = require("./Account");
 
 //test database connection
 try {
@@ -16,97 +11,12 @@ try {
   console.error("Impossible de se connecter, erreur suivante :", error);
 }
 
-//get all users
-app.get("/register", (req, res) =>
-  Account.findAll()
-    .then((accounts) => {
-      console.log(accounts);
-      res.sendStatus(200);
-    })
-    .catch((err) => console.log(err))
-);
+const app = express();
+app.use(require('./routes/routes'));
+const port = 3000; //setup port
+app.use(express.json());
 
-//get one user by ID
-app.get("/register/:id", (req, res) =>
-  Account.findOne({
-    where: { id: req.params.id },
-  })
-    .then((accounts) => {
-      console.log(accounts);
-      res.send(accounts);
-    })
-    .catch((err) => console.log(err))
-);
 
-//delete one user by ID
-app.delete("/register/:id", (req, res) =>
-  Account.destroy({
-    where: { id: req.params.id },
-  })
-    .then((accounts) => {
-      console.log("deleted");
-      res.send("deleted");
-    })
-    .catch((err) => console.log(err))
-);
-
-// update one user by ID
-app.put("/register/:id", (req, res) =>
-  Account.update(
-    {
-      client_firstname: req.body.client_firstname,
-      client_lastname: req.body.client_lastname,
-      client_email: req.body.client_email,
-      client_phonenumber: req.body.client_phonenumber,
-      client_address: req.body.client_address,
-      client_referralcode: req.body.client_referralcode,
-      client_password: req.body.client_password,
-    },
-    {
-      where: { id: req.params.id },
-    }
-  )
-    .then((accounts) => {
-      console.log(accounts);
-      res.send(accounts);
-    })
-    .catch((err) => console.log(err))
-);
-
-//connection to database without sequelize
-const database = mysql.createConnection({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  multipleStatements: true,
-});
-
-//create a new account
-app.post("/register", (req, res) => {
-  var client_firstname = req.body.client_firstname;
-  var client_lastname = req.body.client_lastname;
-  var client_email = req.body.client_email;
-  var client_phonenumber = req.body.client_phonenumber;
-  var client_address = req.body.client_address;
-  var client_referralcode = req.body.client_referralcode;
-  var client_password = req.body.client_password;
-
-  var query = `
-    INSERT INTO accounts 
-    (client_firstname, client_lastname, client_email, client_phonenumber, client_address, client_referralcode, client_password)
-    VALUES ("${client_firstname}", "${client_lastname}", "${client_email}", "${client_phonenumber}", "${client_address}", "${client_referralcode}", "${client_password}")
-    `;
-
-  database.query(query, function (error, results) {
-    if (error) {
-      res.status(404).send("Can't create user");
-    }
-    res.status(200).send("User created");
-  });
-});
-
-//setup port
-const port = 3000;
 // PORT
 app.listen(port, () => {
   console.log(`Up and Running on port ${port} - This is Account service`);
---
+++ microservices/account/routes/routes.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
+require("dotenv").config();
+
+const Account = require("../Account");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+
+//get all users
+router.get("/register", (req, res) =>
+  Account.findAll()
+    .then((accounts) => {
+      console.log(accounts);
+      res.sendStatus(200);
+    })
+    .catch((err) => console.log(err))
+);
+
+//get one user by ID
+router.get("/register/:id", (req, res) =>
+  Account.findOne({
+    where: { id: req.params.id },
+  })
+    .then((accounts) => {
+      console.log(accounts);
+      res.send(accounts);
+    })
+    .catch((err) => console.log(err))
+);
+
+//delete one user by ID
+router.delete("/register/:id", (req, res) =>
+  Account.destroy({
+    where: { id: req.params.id },
+  })
+    .then((accounts) => {
+      console.log("deleted");
+      res.send("deleted");
+    })
+    .catch((err) => console.log(err))
+);
+
+// update one user by ID
+router.put("/register/:id", (req, res) =>
+  Account.update(
+    {
+      client_firstname: req.body.client_firstname,
+      client_lastname: req.body.client_lastname,
+      client_email: req.body.client_email,
+      client_phonenumber: req.body.client_phonenumber,
+      client_address: req.body.client_address,
+      client_referralcode: req.body.client_referralcode,
+      client_password: req.body.client_password,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  )
+    .then((accounts) => {
+      console.log(accounts);
+      res.send(accounts);
+    })
+    .catch((err) => console.log(err))
+);
+
+//create a new account
+router.post("/register", jsonParser, (req, res) => {
+  Account.create(
+    {
+      client_firstname: req.body.client_firstname,
+      client_lastname: req.body.client_lastname,
+      client_email: req.body.client_email,
+      client_phonenumber: req.body.client_phonenumber,
+      client_address: req.body.client_address,
+      client_referralcode: req.body.client_referralcode,
+      client_password: req.body.client_password,
+    }
+  )
+    .then((accounts) => {
+      console.log(accounts);
+      res.send(accounts);
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
